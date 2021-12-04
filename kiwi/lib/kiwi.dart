39683adafcaf811c1a03b608d84934fadd719db6
Hericles Koelher <brunokoelher@hotmail.com>
2021-12-03 23:28:00 -0300

    added kiwi errors export

--- kiwi/lib/kiwi.dart
+++ kiwi/lib/kiwi.dart
@@ -2,3 +2,5 @@ library kiwi;
 
 export 'src/kiwi_container.dart';
 export 'src/annotations.dart';
+export 'src/model/exception/kiwi_error.dart';
+export 'src/model/exception/not_registered_error.dart';
