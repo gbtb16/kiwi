30e830696718d149273f82aeeadb28d0d8fc6406
Koen Van Looveren <koen.vanlooveren@icapps.com>
2023-02-15 19:17:08 +0100

    Updated format tool script

--- tool/format.sh
+++ tool/format.sh
@@ -10,27 +10,23 @@ fi
 echo ""
 echo "example format lib"
 cd example
-flutter packages pub global run dart_style:format -w lib
-flutter packages pub global run dart_style:format -w test
+dart format -o none --set-exit-if-changed .
 cd ..
 
 echo ""
 echo "flutter_example format lib"
 cd flutter_example
-flutter packages pub global run dart_style:format -w lib
-flutter packages pub global run dart_style:format -w test
+dart format -o none --set-exit-if-changed .
 cd ..
 
 echo ""
 echo "kiwi format lib"
 cd kiwi
 dart format -o none --set-exit-if-changed .
-dart format -o none --set-exit-if-changed .
 cd ..
 
 echo ""
 echo "kiwi_generator format lib"
 cd kiwi_generator
-dart format -o none --set-exit-if-changed .
 dart format -o none --set-exit-if-changed .
 cd ..
