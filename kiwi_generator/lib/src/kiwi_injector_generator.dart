fe110bc4fae850985313afd29a40de26c54fb101
Gabriel Barbosa <gabrieltavares.tic@gmail.com>
2024-01-15 12:58:49 -0300

    fix(kiwi-injector): changed were occurs errors in kiwi injector generator.

--- kiwi_generator/lib/src/kiwi_injector_generator.dart
+++ kiwi_generator/lib/src/kiwi_injector_generator.dart
@@ -14,7 +14,7 @@ import 'package:kiwi/kiwi.dart';
 
 const TypeChecker _registerTypeChecker = TypeChecker.fromRuntime(Register);
 
-bool _isRegisterMethod(MethodElement method) => method.returnType.isVoid && _registerTypeChecker.hasAnnotationOfExact(method);
+bool _isRegisterMethod(MethodElement method) => (method.returnType is VoidType && _registerTypeChecker.hasAnnotationOfExact(method));
 
 class KiwiInjectorGenerator extends Generator {
   const KiwiInjectorGenerator();
